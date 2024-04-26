b4979fce6f8d30fac880500758ab036df532b73d
rmandadi1793 <142231662+rmandadi1793@users.noreply.github.com>
2024-04-26 17:32:18 -0400

    Update ErrorViewModel.cs

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -2,7 +2,7 @@ namespace Assignment03.Models
 {
     public class ErrorViewModel
     {
-        public string? RequestId { get; set; }
+        public string? RequestId { get; set }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
